46ca37db3a8e7a834d683abea04a39047de4003b
AkitaMultimedia <info@akitamultimedia.com>
2021-03-23 12:40:12 -0400

    Adapter Entity User

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -70,8 +70,9 @@ class Users implements UserInterface
         $roles = $this->roles;
 
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
+        if (empty($roles)) {
+            array_push($roles,'ROLE_USER');
+        }
         return array_unique($roles);
     }
 
